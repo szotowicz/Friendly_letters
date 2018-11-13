51dda209d97ad63e11c4ee53a615a5f4f436d36c
mszotowicz <mikolaj.szotowicz@gmail.com>
2018-11-13 23:49:05 +0100

    Add project with two clean activities

---
+++ app/src/main/java/com/pg/eti/mikszo/friendlyletters/SettingsMainActivity.java
@@ -0,0 +1,13 @@
+package com.pg.eti.mikszo.friendlyletters;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_main);
+    }
+}
