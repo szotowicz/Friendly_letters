8a0fcc55e3f8cb38021a919f02028e0dcab7651a
mszotowicz <mikolaj.szotowicz@gmail.com>
2018-12-27 21:01:26 +0100

    Add settings. Copy default images from assets

--- app/src/main/java/com/pg/mikszo/friendlyletters/SettingsMainActivity.java
+++ app/src/main/java/com/pg/mikszo/friendlyletters/SettingsMainActivity.java
@@ -1,12 +1,11 @@
 package com.pg.mikszo.friendlyletters;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.pg.mikszo.friendlyletters.drawing.AddingShapeView;
 
-public class SettingsMainActivity extends Activity {
+public class SettingsMainActivity extends BaseActivity {
 
     private AddingShapeView addingShapeView;
 
@@ -15,6 +14,10 @@ public class SettingsMainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_main);
         addingShapeView = findViewById(R.id.adding_shape_view);
+
+        if (!isAppFolderExists()) {
+            copyDefaultImages();
+        }
     }
 
     public void addShapeToResourcesOnClick(View view) {
