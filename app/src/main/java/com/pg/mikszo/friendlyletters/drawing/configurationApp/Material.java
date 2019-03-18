0be2621ead3d118a27f7a724495592e514080b53
mszotowicz <mikolaj.szotowicz@gmail.com>
2019-03-18 17:18:51 +0100

    Optimization of photo loading

--- app/src/main/java/com/pg/mikszo/friendlyletters/drawing/configurationApp/Material.java
+++ app/src/main/java/com/pg/mikszo/friendlyletters/drawing/configurationApp/Material.java
@@ -1,15 +1,16 @@
 package com.pg.mikszo.friendlyletters.drawing.configurationApp;
 
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Material {
-    public Button material;
+    public ImageView material;
     public Button cloneOfBackground;
 
     public Material () {
     }
 
-    public Material (Button material, Button cloneOfBackground) {
+    public Material (ImageView material, Button cloneOfBackground) {
         this.material = material;
         this.cloneOfBackground = cloneOfBackground;
     }
